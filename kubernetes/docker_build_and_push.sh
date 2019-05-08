5dc4538fc10ed7f2828be85d65268f4f829be725
Jan Tattermusch <jtattermusch@google.com>
2019-05-08 09:22:50 -0400

    add docker build and push

---
+++ kubernetes/docker_build_and_push.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Copyright 2019 The gRPC Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -ex
+cd $(dirname $0)
+
+# NOTE: Need to run "gcloud auth configure-docker" before we can push the images to gcr.io
+
+# Build docker images & push them to container registry
+docker build -t gcr.io/grpc-authentication-demo2019/greeter_client ../greeter-client && docker push gcr.io/grpc-authentication-demo2019/greeter_client:latest
+docker build -t gcr.io/grpc-authentication-demo2019/greeter_server ../greeter-server && docker push gcr.io/grpc-authentication-demo2019/greeter_server:latest
+
